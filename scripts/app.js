21a771248080603b44c71bcee28915fb0b2d16c5
qiwen.li <qiwen.li@qunar.com>
2017-02-27 16:00:32 +0800

    fix

--- scripts/app.js
+++ scripts/app.js
@@ -315,6 +315,7 @@
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
             console.log('Service Worker Registered');
+            console.dir(registration);
         });
     }
 })();
