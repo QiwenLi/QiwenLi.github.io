cdcb230149a4e15b2a4fef37a673149bdfb1e473
qiwen.li <qiwen.li@qunar.com>
2017-02-28 11:03:23 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -23,23 +23,32 @@ var filesToCache = [
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] install', e);
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
+        caches
+            .open(cacheName)
+            .then(function (cache) {
+                console.log('[ServiceWorker] Caching app shell');
+                return cache.addAll(filesToCache);
+            })
+            .then(function () {
+                self.skipWaiting();
+            })
     );
 });
 self.addEventListener('activate', function (e) {
     console.log('[ServiceWorker] Activate', e);
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                if (key !== cacheName && key != dataCacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    if (key !== cacheName && key != dataCacheName) {
+                        return caches.delete(key);
+                    }
+                }));
+            })
+            .then(function () {
+                self.clients.claim();
+            })
     );
 });
 self.addEventListener('fetch', function (e) {
